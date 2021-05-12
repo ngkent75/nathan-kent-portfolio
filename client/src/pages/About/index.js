71c1d34b9e25ac09e716520e35e46829873e68df
Nate Kent <ngkent75@gmail.com>
2021-05-12 01:11:12 -0400

    added link to resume. will have to style later. added links to contact page. Will probably turn into big buttons and style later

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -12,39 +12,41 @@ function About() {
 
       <div>
         Hi, my name is Nathan! I am a fresh full-stack web developer who recently graduated from the UNCC coding bootcamp with and A. Outside of coding, I enjoy coaching Pop Warner football, PC building, and playing video games. I really enjoy learning new things, which is part of the reason why I started coding. There's no limit to what you can learn!
-    </div>
+      </div>
 
-    <h2>Hard Skils</h2>
+      <a href="https://drive.google.com/file/d/1GgFXAZCGroPbxsTKXRKMDh7Xn0fU8wgu/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
 
-    <ul>
-      <li>HTML5</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>Bootstrap</li>
-      <li>jQuery</li>
-      <li>Sequelize</li>
-      <li>MySQL</li>
-      <li>MongoDB</li>
-      <li>Node.js</li>
-      <li>Express</li>
-      <li>Session</li>
-      <li>Progressive Web Applications</li>
-      <li>Handlebars</li>
-      <li>React</li>
-    </ul>
-    
-    <h2>Soft Skils</h2>
+      <h2>Hard Skils</h2>
 
-    <ul>
-      <li>Critical Thinking</li>
-      <li>Problem Solving</li>
-      <li>Communication</li>
-      <li>Leadership</li>
-      <li>Collaborative</li>
-      <li>Self-starting</li>
-      <li>Malleable</li>
-      <li>Inquisitive</li>
-    </ul>
+      <ul>
+        <li>HTML5</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Bootstrap</li>
+        <li>jQuery</li>
+        <li>Sequelize</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>Session</li>
+        <li>Progressive Web Applications</li>
+        <li>Handlebars</li>
+        <li>React</li>
+      </ul>
+
+      <h2>Soft Skils</h2>
+
+      <ul>
+        <li>Critical Thinking</li>
+        <li>Problem Solving</li>
+        <li>Communication</li>
+        <li>Leadership</li>
+        <li>Collaborative</li>
+        <li>Self-starting</li>
+        <li>Malleable</li>
+        <li>Inquisitive</li>
+      </ul>
     </>
   );
 }
--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Contact () {
   return (
-    <div>
-      Hello there, this is my Contact page. This will just have useful links to contact me such as email, phone, github, linkedIn
-    </div>
+    <>
+      <h1>Contact Information and Important Links</h1>
+
+      <ul>
+        <li><a href="tel:7043019283" target="_blank" rel="noreferrer">Phone</a></li>
+        <li><a href="mailto:ngkent75@gmail.com" target="_blank" rel="noreferrer">Email</a></li>
+        <li><a href="https://www.linkedin.com/in/NathanGKent" target="_blank" rel="noreferrer">LinkedIn</a></li>
+        <li><a href="https://github.com/ngkent75" target="_blank" rel="noreferrer">Github</a></li>
+      </ul>
+    </>
   );
 }
 
