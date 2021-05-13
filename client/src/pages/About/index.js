706fed210e9d90c0059a249b3bf4e951486ca022
Nate Kent <ngkent75@gmail.com>
2021-05-12 20:59:43 -0400

    added bootstrap columns to about page. Now responsive

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,52 +1,79 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './index.css';
 
 function About() {
   return (
     <>
 
-      <img src="./assets/Portrait.jpg" alt="Portrait of Me" width="300" />
+      <Container>
+        <Row>
+          <Col align="center">
+            <h1>
+              About Me
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6} align="center">
+            <img src="./assets/Portrait.jpg" alt="Portrait of Me" width="300"/>
+          </Col>
+          <Col sm={6} align="center">
+            <div>
+              Hi, my name is Nathan! I am a fresh full-stack web developer who recently graduated from the UNCC coding bootcamp with an A. Outside of coding, I enjoy coaching Pop Warner football, PC building, and playing video games. I really enjoy learning new things, which is part of the reason why I started coding. There's no limit to what you can learn!
+            </div>
+          </Col>
+        </Row>
+        <Row >
+          <Col sm={6} >
+            <div>
+              <h2 align="center">Hard Skils</h2>
 
-      <h1>
-        About Me
-      </h1>
+              <ul>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+                <li>jQuery</li>
+                <li>Sequelize</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>Session</li>
+                <li>Progressive Web Applications</li>
+                <li>Handlebars</li>
+                <li>React</li>
+              </ul>
+            </div>
+          </Col>
+          <Col sm={6}>
+            <div>
+              <h2 align="center">Soft Skils</h2>
 
-      <div>
-        Hi, my name is Nathan! I am a fresh full-stack web developer who recently graduated from the UNCC coding bootcamp with an A. Outside of coding, I enjoy coaching Pop Warner football, PC building, and playing video games. I really enjoy learning new things, which is part of the reason why I started coding. There's no limit to what you can learn!
-      </div>
+              <ul>
+                <li>Critical Thinking</li>
+                <li>Problem Solving</li>
+                <li>Communication</li>
+                <li>Leadership</li>
+                <li>Collaborative</li>
+                <li>Self-starting</li>
+                <li>Malleable</li>
+                <li>Inquisitive</li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
 
       <a href="https://drive.google.com/file/d/1GgFXAZCGroPbxsTKXRKMDh7Xn0fU8wgu/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
 
-      <h2>Hard Skils</h2>
 
-      <ul>
-        <li>HTML5</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Bootstrap</li>
-        <li>jQuery</li>
-        <li>Sequelize</li>
-        <li>MySQL</li>
-        <li>MongoDB</li>
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>Session</li>
-        <li>Progressive Web Applications</li>
-        <li>Handlebars</li>
-        <li>React</li>
-      </ul>
 
-      <h2>Soft Skils</h2>
 
-      <ul>
-        <li>Critical Thinking</li>
-        <li>Problem Solving</li>
-        <li>Communication</li>
-        <li>Leadership</li>
-        <li>Collaborative</li>
-        <li>Self-starting</li>
-        <li>Malleable</li>
-        <li>Inquisitive</li>
-      </ul>
+
+
     </>
   );
 }
